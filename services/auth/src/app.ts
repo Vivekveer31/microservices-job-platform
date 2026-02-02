bd6e591aeea52c0eb8f36dfc5eb0499be899a52f
Vivekveer31 <veervivek47@gmail.com>
2026-02-02 17:24:40 +0530

    chore:initial monorepo structure(frontend+services)

---
+++ services/auth/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+ const app = express();
+
+ app.get("/", (req, res) => {
+   res.send('<h1>Auth Service is running</h1>');
+ });
+
+
+ import authRoutes from "./routes/auth.js";
+ app.use("/api/v1/auth",authRoutes)
+
+  export default app;
---
+++ services/auth/src/controllers/auth.ts
@@ -0,0 +1,9 @@
+import { Request,Response } from "express";
+
+ const  userRegister=(req:Request,res:Response)=>{
+   try {
+    
+   } catch (error:any) {
+     
+   }
+ }
